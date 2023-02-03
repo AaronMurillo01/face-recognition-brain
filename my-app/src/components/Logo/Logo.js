4639a3dfd605d6435ed2fee22bae348f961c6f36
Aaron Murillo <amurillo11@cnm.edu>
2023-02-03 14:08:54 -0700

    adding updated file and code to make animation work

---
+++ my-app/src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+    return(
+        <div className="ma4 mt0">
+            <img alt="logo" src=""/>
+        </div> 
+    );
+}
+
+export default Logo;
